f33653f927e3375ff742429ccdc51bcea1eec8b4
willvo2004 <willvo2004@gmail.com>
2024-02-18 13:36:50 -0500

    added landing page

---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function PersonalDetails({ onSubmit }) {
+    const [fullName, setFullName] = useState('');
+
+    const handleChange = (event) => {
+        setFullName(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        // Call the onSubmit function with the fullName value
+        onSubmit(fullName);
+    };
+
+    return (
+        <div className='wrapper'>
+            <div className="content">
+                <div className="content-text">
+                    Start by entering your full name.
+                </div>
+                <div className="content-input">
+                    <input type="text" placeholder="First and last name" value={fullName} onChange={handleChange} />
+                </div>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        </div>
+    );
+}
